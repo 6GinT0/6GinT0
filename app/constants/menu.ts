8ca8fc953190a6ee8e02105db65e4f330b211274
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2025-09-13 01:25:13 -0300

    chore: tailwind config

--- app/constants/menu.ts
+++
@@ -1,6 +0,0 @@
-export const mainMenu = [
-  { label: "Inicio", to: "/" },
-  { label: "Herramientas", to: "/tools" },
-  { label: "Proyectos", to: "/projects" },
-  { label: "Blog", to: "/blog" }
-];
