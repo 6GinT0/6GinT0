3970889b264b798c7652117b87eb1c9f9d5e832e
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2025-09-13 01:25:45 -0300

    feat: default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const route = useRoute();
+const { t } = useI18n();
+const head = useLocaleHead();
+const title = computed(() => t(route.meta.title));
+
+useHead({
+  titleTemplate: "%s | Ulises Saucedo"
+});
+</script>
+
+<template>
+  <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+    <Head>
+      <Title>{{ title }}</Title>
+      <template v-for="link in head.link" :key="link.key">
+        <Link :id="link.key" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+      </template>
+      <template v-for="meta in head.meta" :key="meta.key">
+        <Meta :id="meta.key" :property="meta.property" :content="meta.content" />
+      </template>
+    </Head>
+    <Body>
+      <Header />
+      <slot />
+    </Body>
+  </Html>
+</template>
