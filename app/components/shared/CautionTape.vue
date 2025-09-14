20f1bd541a2560015ff3e39a798ef3cb5b2ea5a7
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2025-09-14 05:16:26 -0300

    chore: separate functionalities

---
+++ app/components/shared/CautionTape.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  message: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="relative w-full h-[calc(100dvh-100px)]">
+    <div class="bg w-full h-12 flex items-center justify-center">
+      <div class="px-4 py-2 bg-black">{{ message }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg {
+  background-image: linear-gradient(
+    135deg,
+    #ffcc00 25%,
+    #000000 25%,
+    #000000 50%,
+    #ffcc00 50%,
+    #ffcc00 75%,
+    #000000 75%,
+    #000000 100%
+  );
+  background-size: 56.57px 56.57px;
+}
+</style>
