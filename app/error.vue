f1a05d9a2225dd4ae53e7aba81250907aaac3072
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2026-02-22 23:48:20 -0300

    feat(ui): error page handling

--- app/error.vue
+++ app/error.vue
@@ -2,27 +2,22 @@
 import type { NuxtError } from "#app";
 
 const { t } = useI18n();
+const localePath = useLocalePath();
 
-const props = defineProps({
-  error: Object as () => NuxtError
-});
+const props = defineProps<{ error: NuxtError }>();
 
 useHead({
-  title: props.error?.statusCode ?? "404"
+  title: props.error?.status ?? "404"
 });
 </script>
 
 <template>
-  <NuxtLayout>
-    <div
-      class="flex items-center justify-center container mx-auto p-4 h-[calc(100dvh-200px)] flex-col space-y-4"
-    >
-      <h1 class="text-7xl">{{ error?.statusCode ?? "404" }}</h1>
-      <p>{{ t("pages.404.message") }}</p>
+  <div class="flex gap-4 items-center justify-center flex-col h-dvh">
+    <h1 class="text-7xl">{{ error?.status ?? "404" }}</h1>
+    <p>{{ t("404.message") }}</p>
 
-      <NuxtLink to="/" class="bg-primary dark:bg-[var(--terciary)]/25 rounded-full px-4 py-2">
-        {{ t("pages.404.back") }}
-      </NuxtLink>
-    </div>
-  </NuxtLayout>
+    <UButton color="primary" variant="soft" :to="localePath('/')">
+      {{ t("404.back") }}
+    </UButton>
+  </div>
 </template>
